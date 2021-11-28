85df627ff16e596b6ba4709a2bc329be0f0bf559
Harshul Makwana <harshul.makwana@mesur.io>
2021-11-28 14:25:14 -0500

    Change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 let HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    // console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
